421d418d0af4e2c1509c498568b73472939e8654
Andrei Roque <roqueandrei0316@gmail.com>
2026-05-10 20:19:51 +0800

    Update: Add count functionality to app.tsx

--- Simple-counter/src/App.tsx
+++ Simple-counter/src/App.tsx
@@ -1,5 +1,25 @@
+import { useState } from "react";
+import Btn from "./Btn";
+
 const App = () => {
-  return <div>App</div>;
+  const [count, setCount] = useState<number>(0);
+
+  return (
+    <div className="h-screen flex justify-center items-center flex-col gap-10 font-mono">
+      <h1 className="text-5xl">Simple counter</h1>
+      <div className="flex flex-col gap-5">
+        <p className="font-[1.5rem] text-center">Counter: {count}</p>
+        <div className="flex gap-5">
+          <Btn
+            click={() => setCount(count + 1)}
+            style="border rounded-md px-5 py-3 cursor-pointer"
+          >
+            Add
+          </Btn>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
