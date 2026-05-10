f55babd81df24216fa27a90bb4f87483b38491dc
Andrei Roque <roqueandrei0316@gmail.com>
2026-05-10 20:15:05 +0800

    Add new file: Added Btn.tsx

---
+++ Simple-counter/src/Btn.tsx
@@ -0,0 +1,15 @@
+interface BtnProps {
+  click: () => void;
+  children: string;
+  style?: string;
+}
+
+const Btn = ({ click, children, style }: BtnProps) => {
+  return (
+    <button onClick={click} className={style}>
+      {children}
+    </button>
+  );
+};
+
+export default Btn;
