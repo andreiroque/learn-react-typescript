cc75e86ab4e2658694465cf1e1b2bacd130d328c
Andrei Roque <roqueandrei0316@gmail.com>
2026-05-15 14:07:31 +0800

    Add: All functionalities

--- Todo-list/src/App.tsx
+++ Todo-list/src/App.tsx
@@ -4,6 +4,17 @@ const App = () => {
   const [todo, setTodo] = useState<string[]>([]);
   const [task, setTask] = useState<string>("");
 
+  const handleAddTask = () => {
+    if (task.length > 0) {
+      setTodo([...todo, task]);
+    }
+    setTask("");
+  };
+
+  const handleTaskDone = (task: string) => {
+    setTodo(todo.filter((t) => t !== task));
+  };
+
   return (
     <div className="h-screen flex items-center justify-center">
       <div className="h-96 w-72 shadow-xl/30 rounded-xl p-5 text-mono flex flex-col gap-10">
@@ -14,8 +25,13 @@ const App = () => {
               type="text"
               placeholder="Enter a task"
               className="border border-slate-500 rounded-md p-1"
+              onChange={(e) => setTask(e.target.value)}
+              value={task}
             />
-            <button className="bg-slate-900 py-1 px-2 rounded-md text-white cursor-pointer">
+            <button
+              onClick={() => handleAddTask()}
+              className="bg-slate-900 py-1 px-2 rounded-md text-white cursor-pointer"
+            >
               Add
             </button>
           </div>
@@ -23,7 +39,15 @@ const App = () => {
         <div className="p-5">
           <ul className="list-disc">
             {todo.map((t) => (
-              <li key={t}>{t}</li>
+              <li key={t}>
+                {t}{" "}
+                <input
+                  type="checkbox"
+                  name="done"
+                  id="done"
+                  onChange={() => handleTaskDone(t)}
+                />
+              </li>
             ))}
           </ul>
         </div>
